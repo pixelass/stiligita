557962d2caab4ff49a8e5f372a3570ef52c47fb8
Gregor Adams <greg@pixelass.com>
2017-12-05 19:14:58 +0100

    refactor: clean code, rename identifiers

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {createElement, Component} from 'react'
+import React, {createElement} from 'react'
 
 import domElements, {htmlAttributes, starAttributes} from './dom-elements'
 import Store from './store'
@@ -8,26 +8,26 @@ import {createStyleBlock, createCSS} from './css'
 
 const styled = new Store()
 
-domElements.forEach(domElement => {
-  styled[domElement] = (strings, ...args) => createComponent(strings, args, domElement)
+domElements.forEach(tag => {
+  styled[tag] = (strings, ...args) => createComponent(strings, args, tag)
 })
 
 const getInvalidProperties = props => {
-  const elementAttributes = htmlAttributes[props.domElement] || []
+  const elementAttributes = htmlAttributes[props.tag] || []
   const validAttributes = [...starAttributes, ...elementAttributes, 'children']
   return Object.keys(props).filter(prop => validAttributes.indexOf(prop) < 0 && !isListener(prop))
 }
 
-const Stiligita = props => createElement(props.domElement, {...cleanObject(props, getInvalidProperties(props)), className: props.className})
+const Stiligita = props => createElement(props.tag, {...cleanObject(props, getInvalidProperties(props)), className: props.className})
 
-const createComponent = (strings, args, domElement) => props => {
+const createComponent = (strings, args, tag) => props => {
     const css = createCSS(strings, args, props)
     const className = hashCode(css)
     styled.__STYLES__[className] = css
     if (!styled.__STYLE_TAG__.innerHTML.match(`.${className}{`)) {
       styled.__STYLE_TAG__.innerHTML = createStyleBlock(styled.__STYLES__, styled.__KEYFRAMES__)
     }
-    return <Stiligita {...props} className={className} domElement={domElement}/>
+    return <Stiligita {...props} className={className} tag={tag}/>
   }
 
 export const Keyframes = strings => {
