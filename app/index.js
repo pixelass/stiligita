14823c30a9c81df3a435c6aa5a202bb818ebe2b0
Gregor Adams <greg@pixelass.com>
2017-12-05 03:47:25 +0100

    chore(gh-pages): demo

--- app/index.js
+++ app/index.js
@@ -1,31 +1,46 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
-import styled from '../src'
+import styled, {Keyframes} from '../src'
+
+const spin = Keyframes`to {transform: rotate(360deg);}`
 
 const Header = styled.header`
-  padding: 3em 5em;
-  background: ${_ => _.active ? 'blue' : 'red'};
-  color: white;
+  padding: 8em 5em;
+  background: ${_ => _.active ? '#fe9f2e' : '#63e2bb'};
+  color: black;
   transition: background 1s;
+  position: relative;
 
   &:hover {
-    background: ${_ => _.active ? 'green' : 'purple'};
+    background: ${_ => _.active ? '#ae6f22' : '#49a489'};
+    .ball {
+     font-size: 10em;
+    }
   }
-`
 
-const Footer = styled.footer`
-padding: 2em;
-  ${p => p.dark ? `
-    background: black;
-    color: white;
-  ` : `
-    background: yellow;
-    color: #444;
-  `}
-`
+  .ball {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+    top: 50%;
+    left: 50%;
+    margin: -0.5em;
+    height: 1em;
+    width: 1em;
+    font-size: 5em;
+    background: white;
+    position: relative;
+    transform: rotate(0deg);
+    animation: ${spin} 2s linear infinite;
+    animation-direction: ${_ => _.active ? 'normal' : 'reverse'};
 
-const Img = styled.img`
-  width: ${p => p.imageWidth || 400}px
+    &:before {
+      font-size: 0.7em;
+      content: "${_ => _.active ? '💖' : '😂'}";
+    }
+  }
 `
 
 class App extends Component {
@@ -45,13 +60,8 @@ class App extends Component {
         <Header onClick={this.handleClick}
                 active={this.state.active}>
           Stiligita
+          <div className='ball'></div>
         </Header>
-        <Footer dark>Footer</Footer>
-        <Footer>Footer</Footer>
-        <Img src='//placehold.it/800' imageWidth={100}/>
-        <Img src='//placehold.it/800' imageWidth={100}/>
-        <Img src='//placehold.it/800' imageWidth={200}/>
-        <Img src='//placehold.it/800' imageWidth={200}/>
       </div>
     )
   }
