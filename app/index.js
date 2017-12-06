3c7b202ce9e9b602b377e0449cbdc7dae96f75d7
Gregor Adams <greg@pixelass.com>
2017-12-06 11:51:31 +0100

    chore(gh-pages): new version with custom processor

--- app/index.js
+++ app/index.js
@@ -1,14 +1,39 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
-import styled, {Keyframes} from '../src'
+import Stylis from 'stylis'
+import styled, {Keyframes, render as renderStyled} from '../src'
+
+renderStyled.processor = new Stylis({keyframe: false})
 
 const spin = Keyframes`to {transform: rotate(360deg);}`
 
+const theme = {
+  primary: '#ff0080',
+  light: '#ccff66',
+  dark: '#008000',
+  lightText: '#fff',
+  darkText: '#000'
+}
+
+const Wrapper = styled.div`
+  background: ${theme.light};
+  color: ${theme.darkText};
+  display: grid;
+  grid-template-rows: 100px auto 100px;
+  grid-template-areas: "header" "main" "footer";
+  min-height: calc(100vh - 1em);
+`
+
 const Header = styled.header`
-  user-select: none;
-  background: ${_ => _.active ? '#000' : '#fff'};
-  color: ${_ => _.active ? '#fff' : '#000'};
-  cursor: pointer;
+  grid-area: header;
+  background: ${theme.dark};
+  color: ${theme.lightText};
+`
+
+const Footer = styled.footer`
+  grid-area: footer;
+  background: ${theme.dark};
+  color: ${theme.lightText};
 `
 
 const Spinner = styled.span`
@@ -17,11 +42,27 @@ const Spinner = styled.span`
   animation: ${spin} 2s linear infinite;
   animation-direction: ${_ => _.active ? 'normal' : 'reverse'};
 `
+
 const Title = styled.h1`
+  user-select: none;
   font-family: 'Comic Sans MS';
   font-size: 4em;
   margin: 0;
   text-align: center;
+`
+
+const Button = styled.button`
+  background: ${props => props.primary ? '#66ffff' : '#b3b3b3'};
+  color: black;
+  padding: 1em 2em;
+  line-height: 1;
+  font-size: 1em;
+  border: 0;
+  border-radius: 1.5em;
+
+  &:hover {
+    background: ${props => props.primary ? '#66ccff' : '#999999'};
+  }
 `
 
 class App extends Component {
@@ -37,16 +78,20 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
-        <Header onClick={this.handleClick}
-                active={this.state.active}
-                data-foo="I should work">
+      <Wrapper>
+        <Header data-foo="I should work">
           <Title aria-label="Click me to change the state">
+            <Spinner active={this.state.active}>🔫</Spinner>
+            Stiligita
             <Spinner active={this.state.active}>🌀</Spinner>
-            Click me
           </Title>
         </Header>
-      </div>
+        <main>
+          <Button onClick={this.handleClick} primary>Switch direction</Button>
+          <Button onClick={this.handleClick}>Switch direction</Button>
+        </main>
+        <Footer>© 2017 | Gregor Adams greg@pixelass.com</Footer>
+      </Wrapper>
     )
   }
 }
